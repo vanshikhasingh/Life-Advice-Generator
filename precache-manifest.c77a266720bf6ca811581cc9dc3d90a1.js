e7b12cea505270f3f8b0d26bef0a2cb46a3e87ef
Vanshikha Singh <svanshikha8@gmail.com>
2020-10-08 00:52:52 +0530

    Updates

---
+++ precache-manifest.c77a266720bf6ca811581cc9dc3d90a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d985c71d76621a8f2108683135d4fd0",
+    "url": "/vanshikhasingh.github.io/Life-Advice-Generator/index.html"
+  },
+  {
+    "revision": "e86dc2487d7a71910869",
+    "url": "/vanshikhasingh.github.io/Life-Advice-Generator/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "e9a69c67c00216f3869a",
+    "url": "/vanshikhasingh.github.io/Life-Advice-Generator/static/css/main.f21620bb.chunk.css"
+  },
+  {
+    "revision": "e86dc2487d7a71910869",
+    "url": "/vanshikhasingh.github.io/Life-Advice-Generator/static/js/2.55ce6f3e.chunk.js"
+  },
+  {
+    "revision": "bfd04eb851fbc8ea43c375cc9d698376",
+    "url": "/vanshikhasingh.github.io/Life-Advice-Generator/static/js/2.55ce6f3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9a69c67c00216f3869a",
+    "url": "/vanshikhasingh.github.io/Life-Advice-Generator/static/js/main.87dd7e28.chunk.js"
+  },
+  {
+    "revision": "accf93658adb10051804",
+    "url": "/vanshikhasingh.github.io/Life-Advice-Generator/static/js/runtime-main.474edcb8.js"
+  }
+]);
